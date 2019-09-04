fb38975d7ecca99927ad709de4f736078c45ee42
pjt3591oo <pjt3591oo@gmail.com>
2019-09-04 09:35:12 +0900

    function 기반에서 class기반으로 변경한다

--- crawler.py
+++ crawler.py
@@ -2,39 +2,61 @@ import requests as rq
 from bs4 import BeautifulSoup
 import time
 
-BASE_URL = "https://www.google.com/search?"
-QUERY_STR = "q=%s&start=%d"
+class Google:
 
-custom_headers = {
-  'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1.1;)',
-}
+  def __init__(self):
+    self.BASE_URL = "https://www.google.com/search?"
+    self.QUERY_STR = "q=%s&start=%d"
 
-keyword = "python"
-page = 0
-cnt = 0
+    self.custom_headers = {
+      'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 5.1.1;)',
+    }
 
-while True:
-  page = 10*cnt + 1
-  print('============= %d ============='%(page))
-  res = rq.get(BASE_URL + QUERY_STR%("python", page), headers=custom_headers)
-  soup = BeautifulSoup(res.content, 'lxml')
+    self.keyword = "python"
+    self.page = 0
+    self.cnt = 0
 
-  contents = soup.select('.srg > div')
-
-  for content in contents:
-    # print(content)
-    top = content.select('div')[1].text
-    splited_top = top.split('›')
+  def crawler(self):
+    self.page = 10 * self.cnt + 1
     
-    if len(splited_top) > 1:
-      title = ' '.join(splited_top[1:])
-      link = splited_top[0]
-      desc = content('div')[4].text
+    print('============= %d ============='%(self.page))
 
-      print('link: %s'%(link))
-      print('title: %s'%(title))
-      print('desc: %s'%(desc))
+    res = rq.get(
+      self.BASE_URL + self.QUERY_STR%("python", self.page), 
+      headers=self.custom_headers
+    )
+    soup = BeautifulSoup(res.content, 'lxml')
+    contents = soup.select('.srg > div')
+
+    temp = []
+
+    for content in contents:
+      top = content.select('div')[1].text
+      splited_top = top.split('›')
+      
+      if len(splited_top) > 1:
+        title = ' '.join(splited_top[1:])
+        link = splited_top[0]
+        desc = content('div')[4].text
+
+        temp.append({
+          'link': link,
+          'title': title,
+          'desc': desc
+        })
+    
+    self.cnt += 1
+    return temp
+
+if __name__== "__main__":
+  google = Google()
+
+  while True:
+    result = google.crawler()
+
+    for item in result:
+      print('link: %s'%(item["link"]))
+      print('title: %s'%(item["title"]))
+      print('desc: %s'%(item["desc"]))
       print()
-  
-  cnt += 1
-  time.sleep(0.3)
+    time.sleep(0.3)
